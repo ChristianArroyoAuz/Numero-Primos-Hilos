b8b829704e0112c742c7d41bd98092b6f19ef72b
ChristianArroyoAuz <christian.xavier.arroyo.auz@hotmail.com>
2020-02-05 01:19:17 -0500

    Números primos con hilos.
    Impresión de los números primos, multiplos de 7 y multiplos de 9 en el intervalos de -100 a 0. Se hace uso de hilos.

---
+++ Numeros/Numeros/Numeros.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace Numeros
+{
+    class Numeros
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer Hilo: Números Primos, en el rango de -100 a 0.");
+            Thread primerHilo = new Thread(Primos);
+            primerHilo.Start();
+            primerHilo.Join();
+            Console.WriteLine("--------- Presione una Tecla Para Continuar ---------");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Segundo Hilo: Números Multiples de Siete, en el rango de -100 a 0.");
+            Thread segundoHilo = new Thread(MultiplosSiete);
+            segundoHilo.Start();
+            segundoHilo.Join();
+            Console.WriteLine("--------- Presione una Tecla Para Continuar ---------");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Tercer Hilo: Números Multiples de Nueve, en el rango de -100 a 0.");
+            Thread tercerHilo = new Thread(MultiplosNueve);
+            tercerHilo.Start();
+            tercerHilo.Join();
+            Console.WriteLine("------------------ Fin de Los Hilos -----------------");
+            Console.ReadKey();
+        }
+
+        static void Primos()
+        {
+            int contador = 0;
+            for (int i = -100; i <= -2; i++)
+            {
+                for (int j = -100; j <= -1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        contador = contador + 1;
+                    }
+                }
+                if (contador == 2)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    Console.WriteLine(i);
+                }
+                contador = 0;
+            }
+        }
+
+        static void MultiplosSiete()
+        {
+            for (int i = -100; i < 0; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void MultiplosNueve()
+        {
+            for (int i = -100; i < 0; i++)
+            {
+                if (i % 9 == 0)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Numeros/Numeros/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+
+[assembly: Guid("d7871192-2cfe-4a68-814c-37489f896f64")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Numeros")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Numeros")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
